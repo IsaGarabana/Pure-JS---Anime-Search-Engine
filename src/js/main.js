7f4d72dcf2019b97e167e7b7f5e8d447a6d5ceec
IsaGarabana <isinhagb@gmail.com>
2021-12-29 16:22:12 +0100

    add new js functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-////////// VARIABLES GLOBALES //////////
+/////////////////////////  VARIABLES GLOBALES /////////////////////////
 const searchResult = document.querySelector(".js-searchResult");
 const apiUrl = "https://api.jikan.moe/v3/search/anime?q=";
 const searchInput = document.querySelector(".js-input");
@@ -8,29 +8,14 @@ const searchBtn = document.querySelector(".js-searchBtn");
 
 ///Array de datos del fetch///
 let data = [];
+let dataFavorites = [];
 
-////////// FUNCIONES //////////
-function handleSearchButton(ev) {
-	ev.preventDefault();
-	const inputValue = searchInput.value;
+///////////////////////// FUNCIONES /////////////////////////
 
-	//api only accepts input of >=3 characters
-	if (inputValue.length > 2) {
-		fetch(`${apiUrl}${inputValue}`)
-			.then((response) => response.json())
-			.then((dataApi) => {
-				data = dataApi.results;
-				renderAnimeList();
-			});
-	} else {
-		console.log("Search parameter must contain at least 3 characters");
-	}
-}
-
+////Función que pinta UN elemento del Array////
 function renderAnime(dataAnime) {
 	console.log(dataAnime);
-
-	searchResult.innerHTML += `<li class="">
+	searchResult.innerHTML += `<li class="js_addToFav">
 			<img
 				src="${dataAnime.image_url}"
 				class=""
@@ -39,54 +24,44 @@ function renderAnime(dataAnime) {
 		</li>`;
 }
 
+////Función que recorre el array pintando TODOS sus elementos////
 function renderAnimeList() {
 	for (const eachAnime of data) {
 		renderAnime(eachAnime);
 	}
+
+	const animesListForFavs = document.querySelectorAll(".js_addToFav");
+	console.log(animesListForFavs);
+	for (const eachAnimeForFavs of animesListForFavs) {
+		eachAnimeForFavs.addEventListener("click", handleAddtoFavs);
+	}
 }
+
+//// Función Manejadora del botón Search////
+function handleSearchButton(ev) {
+	ev.preventDefault();
+	const inputValue = searchInput.value;
+
+	//api sólo acepta búsquedas de 3 caracteres o más
+	if (inputValue.length > 2) {
+		fetch(`${apiUrl}${inputValue}+ &limit=3`)
+			.then((response) => response.json())
+			.then((dataApi) => {
+				data = dataApi.results;
+				renderAnimeList();
+			});
+	} else {
+		alert("Please type at least 3 characters to start searching");
+	}
+}
+
+////Función que añade elementos a la lista de favoritos///
+function handleAddtoFavs(event) {
+	console.log(event.currentTarget);
+}
+
+////Función que pinta los favoritos////
 
 ////////// LISTENERS//////////
 
 searchBtn.addEventListener("click", handleSearchButton);
-
-////PRIMER ATTEMPT DE PROGRAMACIÓN/////
-// ////////// VARIABLES GLOBALES //////////
-
-// const animeInput = document.querySelector(".js-input");
-// const searchBtn = document.querySelector(".js-searchBtn");
-// const apiUrl = "https://api.jikan.moe/v3/search/anime?q=";
-// const searchResult = document.querySelector(".js-searchResult");
-
-// let animeArray = {};
-
-// ////////// FUNCIÓN QUE RECOGE LOS RESULTADOS DEL API //////////
-
-// function handleSearchButton(ev) {
-// ev.preventDefault();
-// const inputValue = animeInput.value;
-//if (animeInput.value.lenght => 3) {
-// fetch(`${apiUrl}${inputValue}&limit=3`)
-// .then((response) => response.json())
-// .then((data) => {
-// const animeData = data.results;
-// console.log(animeData);
-// for (const eachAnime of animeData) {
-// const animeTitle = eachAnime.title;
-// const animeImage = eachAnime.image_url;
-// animeArray = { animeTitle, animeImage };
-// console.log(animeArray);
-// }
-// })
-// };
-
-// //pintamos los resultados en nuestro listado
-// }
-
-// ////////// LISTENERS//////////
-
-// /* Listener de búsqueda */ searchBtn.addEventListener(
-// "click",
-// handleSearchButton
-// );
-// /* Listener para añadir favoritos */
-// /* Listener de reset */
